5c31e34d79a085333c003d16f2c997a3e4059f29
ardiprasetiyo <ardiprasetiyo@students.smkn3bandung.sch.id>
2019-12-19 13:08:50 +0700

    Adding Some Changes Endpoint And Request Validation

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// Node Module
+// MODULES
 const app = require('express')()
 const http = require('http').createServer(app)
-const bodyParser = require('body-parser')
 const io = require('socket.io')(http)
-const dateFormat = require('dateformat')
 const fs = require('fs')
+const bodyParser = require('body-parser')
+const dateFormat = require('dateformat')
 
-// Force HTTPS Redirection ( Heroku Only )
+
+// FORCE HTTPS (HEROKU)
 app.use(function(req, res, next) {
     if (process.env.NODE_ENV === 'production') {
         if (req.headers['x-forwarded-proto'] != 'https') {
@@ -17,49 +18,98 @@ app.use(function(req, res, next) {
     } else {
         return next();
     }
-});
+})
 
-// Using Middleware
+
+// SOCKET CONNECTION
+io.on('connection', function(socket){
+	console.log('A User Has Been Connected');
+	socket.on('rfid-reader', function(msg){
+		io.emit('rfid-reader', msg);
+	})
+
+})
+
+
+
+// MIDDLEWARE
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false }))
 
 
-// Routing
+
+// ROUTING
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/api/send', function(req,res){
-	const idRfid = req.body.id_rfid
-	var dateNow  =  dateFormat(new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' }), "d mmmm yyyy H:M:ss")
-	JSONResponse =  {'id_rfid' : idRfid, 'datetime' : dateNow}
 
-	io.emit('test',JSONResponse)
-	res.send(JSONResponse)
+// READ RFID DATA
+
+app.post('/api/rfid', function(req,res){
+	console.log(req.connection.remoteAddress)
+	const rfidID = req.body.id_rfid
+	const deviceID = req.body.id_device
+
+	if( !rfidID || !deviceID ){
+		JSONResponse =  {'status' : 'Error', 'message' : 'Parameter Is Empty'}
+		res.status(500).send(JSONResponse)
+		return;
+	}
+
+	var dateNow  =  dateFormat(new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' }), "d mmmm yyyy H:M:ss")
+
+	var db = JSON.parse(fs.readFileSync('./faker-db.json')) // faker-db Connect
+	// Searching Data  
+	var result = false
+	db.forEach(e => {
+		if( e.deviceID === deviceID ){
+			result = true
+		}
+	})
+
+	// If Not Registered
+	if( !result ){
+		JSONResponse =  {'status' : 'Error', 'message' : 'Device ID Does Not Registered'}
+		res.status(403).send(JSONResponse)
+		return;
+	}
+
+	JSONResponse =  {'id_rfid' : rfidID, 'id_device' : deviceID, 'datetime' : dateNow}
+	io.emit('rfid-reader',JSONResponse)
+	res.status(200).send(JSONResponse)
 })
 
-app.post('/api/register/device', function(req, res){
+
+ // REGISTER DEVICE
+
+
+app.post('/api/device', function(req, res){
 	const deviceID = req.body.id_device
 	const deviceName = req.body.name_device
 
-	// Store Data To Fak Database
-	var db = JSON.parse(fs.readFileSync('./registered-device.json'))
-	db.push({'deviceID' : deviceID, 'deviceName' : deviceName})
-	fs.writeFileSync('./registered-device.json', JSON.stringify(db));
+	if( !deviceID ){
+		JSONResponse =  {'status' : 'Error', 'message' : 'Device ID Is Required'}
+		res.status(422).send(JSONResponse)
+		return;
+	}
 
-	res.send(db)
+	if( !deviceName ){
+		JSONResponse =  {'status' : 'Error', 'message' : 'Device Name Is Required'}
+		res.status(422).send(JSONResponse)
+		return;
+	}
+
+	var db = JSON.parse(fs.readFileSync('./faker-db.json'))
+	db.push({'deviceID' : deviceID, 'deviceName' : deviceName}) // Store Data To Fak Database
+	fs.writeFileSync('./faker-db.json', JSON.stringify(db));
+	JSONResponse = {'status' : 'Success', 'message' : 'Registrasi Device Berhasil'}
+
+	res.status(200).send(JSONResponse)
 })
 
 
-// Socket IO Connection
-io.on('connection', function(socket){
-	console.log('A User Has Been Connected');
-	socket.on('test', function(msg){
-		io.emit('test', msg);
-	})
-
-})
 
 http.listen(process.env.PORT || 3000, function(){
 	console.log("Server Is Listening")
