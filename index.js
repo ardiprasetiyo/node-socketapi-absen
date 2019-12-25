52eb4466764f00e476f86b18b929f6276b6430dc
ardiprasetiyo <ardiprasetiyo@students.smkn3bandung.sch.id>
2019-12-25 08:59:58 +0700

    Removing FakerDB and SomeChanges

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 const dateFormat = require('dateformat')
 
 
-// FORCE HTTPS (HEROKU)
+// FORCE HTTP (HEROKU)
 app.use(function(req, res, next) {
     if (process.env.NODE_ENV === 'production') {
         if (req.headers['x-forwarded-proto'] != 'https') {
@@ -19,6 +19,7 @@ app.use(function(req, res, next) {
         return next();
     }
 })
+
 
 
 // SOCKET CONNECTION
@@ -48,35 +49,20 @@ app.get('/', function(req,res){
 // READ RFID DATA
 
 app.post('/api/rfid', function(req,res){
-	console.log(req.connection.remoteAddress)
+	console.log(fakerHelper)
 	const rfidID = req.body.id_rfid
 	const deviceID = req.body.id_device
 
 	if( !rfidID || !deviceID ){
-		JSONResponse =  {'status' : 'Error', 'message' : 'Parameter Is Empty'}
-		res.status(500).send(JSONResponse)
-		return;
+		JSONResponse =  {'status_code' : 422, 'message' : 'Parameter is Required'}
+		res.status(422).send(JSONResponse)
+		return
 	}
 
 	var dateNow  =  dateFormat(new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' }), "d mmmm yyyy H:M:ss")
+	
+	JSONResponse =  {'status_code' : 200, 'id_rfid' : rfidID, 'id_device' : deviceID, 'datetime' : dateNow}
 
-	var db = JSON.parse(fs.readFileSync('./faker-db.json')) // faker-db Connect
-	// Searching Data  
-	var result = false
-	db.forEach(e => {
-		if( e.deviceID === deviceID ){
-			result = true
-		}
-	})
-
-	// If Not Registered
-	if( !result ){
-		JSONResponse =  {'status' : 'Error', 'message' : 'Device ID Does Not Registered'}
-		res.status(403).send(JSONResponse)
-		return;
-	}
-
-	JSONResponse =  {'id_rfid' : rfidID, 'id_device' : deviceID, 'datetime' : dateNow}
 	io.emit('rfid-reader',JSONResponse)
 	res.status(200).send(JSONResponse)
 })
@@ -90,23 +76,17 @@ app.post('/api/device', function(req, res){
 	const deviceName = req.body.name_device
 
 	if( !deviceID ){
-		JSONResponse =  {'status' : 'Error', 'message' : 'Device ID Is Required'}
+		JSONResponse =  {'status_code' : 422, 'message' : 'Device ID Is Required'}
 		res.status(422).send(JSONResponse)
-		return;
+		return
 	}
 
-	if( !deviceName ){
-		JSONResponse =  {'status' : 'Error', 'message' : 'Device Name Is Required'}
-		res.status(422).send(JSONResponse)
-		return;
-	}
 
-	var db = JSON.parse(fs.readFileSync('./faker-db.json'))
-	db.push({'deviceID' : deviceID, 'deviceName' : deviceName}) // Store Data To Fak Database
-	fs.writeFileSync('./faker-db.json', JSON.stringify(db));
-	JSONResponse = {'status' : 'Success', 'message' : 'Registrasi Device Berhasil'}
+	JSONResponse = {'status_code' : 200, 'message' : 'Device Registered' ,'id_device' : deviceID}
 
 	res.status(200).send(JSONResponse)
+
+
 })
 
 
